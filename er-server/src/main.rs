db0ab2ad249037a524c9283cf379de604aab7503
Viktor Müller <muller.viktor.muller@gmail.com>
2024-03-11 18:44:26 +0100

    Expose port 8080 for the gcloud

--- er-server/src/main.rs
+++ er-server/src/main.rs
@@ -35,7 +35,7 @@ async fn main() {
     let router = build_router();
 
     // run it
-    let listener = tokio::net::TcpListener::bind("0.0.0.0:3000")
+    let listener = tokio::net::TcpListener::bind("0.0.0.0:8080")
         .await
         .unwrap();
     println!("listening on {}", listener.local_addr().unwrap());
